2f6f97d942256fcc42107da3241a6a25742270bd
Kirisame <gensky377@gmail.com>
2023-10-13 00:56:46 +0900

    add new script

---
+++ export-pdf-bookmark.js
@@ -0,0 +1,14 @@
+function printBookmarks(bookmarkList, level) {
+  level = level || 0;
+  var indent = Array(level + 1).join("  ");
+
+  if (bookmarkList.children) {
+    for (var i = 0; i < bookmarkList.children.length; i++) {
+      var bookmark = bookmarkList.children[i];
+      console.println(indent + bookmark.name);
+      printBookmarks(bookmark, level + 1);
+    }
+  }
+}
+
+printBookmarks(this.bookmarkRoot);
