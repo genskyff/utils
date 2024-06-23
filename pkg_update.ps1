a46d47bffdef9ec5e113f24ac2135314ac493ad8
Kirisame <gensky377@gmail.com>
2024-06-23 18:05:11 +0900

    fix pkg update

--- pkg_update.ps1
+++ pkg_update.ps1
@@ -1,9 +1,18 @@
+$ErrorActionPreference = "Stop"
+
+Write-Host "==> Updating scoop package..." -ForegroundColor Cyan
 scoop update -a
 scoop cleanup -a
-rustup update stable
+
+Write-Host "==> Updating rust package..." -ForegroundColor Cyan
+rustup update
 cargo install-update -a
 cargo cache -a
+
+Write-Host "==> Updating gem package..." -ForegroundColor Cyan
 gem update
 gem cleanup
+
+Write-Host "==> Updating npm package..." -ForegroundColor Cyan
 npm outdated -g
 npm update -g
