e676886c892cfc035a7c81f161e4e7f1173171d2
Kirisame <gensky377@gmail.com>
2024-07-13 00:25:55 +0900

    chore: remove gem

--- pkg_update.ps1
+++ pkg_update.ps1
@@ -9,10 +9,6 @@ rustup update
 cargo install-update -a
 cargo cache -a
 
-Write-Host "==> Updating gem package..." -ForegroundColor Cyan
-gem update
-gem cleanup
-
 Write-Host "==> Updating npm package..." -ForegroundColor Cyan
 npm outdated -g
 npm update -g
