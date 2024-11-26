ec0cc00bbde1b1699fb312d5dfbe7093d0cdc64a
Kirisame <gensky377@gmail.com>
2024-11-26 23:10:30 +0900

    feat(check_flags): add options parameter to customize position check

--- lib/check_flags.ts
+++ lib/check_flags.ts
@@ -5,14 +5,18 @@ interface CheckResult {
   messages: string[];
 }
 
-export const checkFlags = (flags: Args): CheckResult => {
+interface Options {
+  position?: number;
+}
+
+export const checkFlags = (flags: Args, options?: Options): CheckResult => {
   const result: CheckResult = {
     error: false,
     messages: [],
   };
 
   for (const key in flags) {
-    if (key === "_" && flags._.length > 0) {
+    if (key === "_" && flags._.length > (options?.position || 0)) {
       result.error = true;
       flags._.forEach((arg) =>
         result.messages.push(`Error: Unexpected argument '${arg}'`)
